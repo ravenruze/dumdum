b9619c47a58fcc5d529135743d98149785bc8a2b
ruze <6525ruze@gmail.com>
2026-04-23 19:43:34 +0700

    tes ubah uinya template laravel

---
+++ resources/views/depan.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Register</title>
+</head>
+<body>
+
+    <h2>Form Registrasi</h2>
+    
+    <form action="#" method="POST">
+        <div>
+            <label for="nama">Nama:</label><br>
+            <input type="text" id="nama" name="nama" required>
+        </div>
+        <br>
+        <div>
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <br>
+        <button type="submit">Register</button>
+    </form>
+
+</body>
+</html>
