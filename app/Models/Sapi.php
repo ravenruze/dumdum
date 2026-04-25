6e49cb2cdc9a1a6bfdc844f610bc8dddf098e96e
ruze <6525ruze@gmail.com>
2026-04-26 01:41:44 +0700

    model : bikin model tabel utama sapi

---
+++ app/Models/Sapi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sapi extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'kode_sapi',
+        'jenis_sapi',
+        'bobot',
+        'harga_jual',
+        'status',
+        'foto_path'
+    ];
+}
